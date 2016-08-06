7c5ff7273decf284ed7f1da51843d7cf6112460a
Barnaby Colby <barnaby.colby@gmail.com>
2016-08-06 17:51:18 +0000

    Modularised Gruntfile.js using load-grunt-config plugin.

---
+++ grunt/jshint.js
@@ -0,0 +1,11 @@
+module.exports = {
+    all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+    ],
+    options: {
+        jshintrc: '.jshintrc',
+        reporterOutput: ""
+    }
+};
